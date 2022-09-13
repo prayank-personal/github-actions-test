d79d455fddfa6754519e853a5924c96da67b494a
prayank-personal <109146668+prayank-personal@users.noreply.github.com>
2022-09-13 18:10:49 +0530

    Update provision-ec2.tf

--- ec2-terraform/provision-ec2.tf
+++ ec2-terraform/provision-ec2.tf
@@ -1,34 +1,60 @@
-# use ubuntu 20 AMI for EC2 instance
-data "aws_ami" "ubuntu" {
-    most_recent = true
-filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
-    }
-filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
+//########################### Common
 data "aws_region" "Region" {
 
 }
 
 locals {
-  vpc_name = "Development-VPC"
+  vpc_name = "${var.prefix}-VPC"
 }
 
 
-# provision to eu-west-1 region
-provider "aws" {
-  region  = "eu-west-1"
-}
-resource "aws_instance" "GithubActionsEC2" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  key_name      = "GithubActions"
-tags = {
-    Name = "GithubActionsEC2"
+####### VM Image to be used 
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2*"]
   }
+filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+owners = ["amazon"]
+}
+
+
+
+resource "aws_instance" "GithubActions" {
+  ami           = "${data.aws_ami.amazon-linux-2.id}"
+  instance_type = "${var.ec2_instance_type}"
+  
+  availability_zone ="${data.aws_region.Region.name}a"
+  vpc_security_group_ids = [
+    "${var.security_group}",
+  ]
+  subnet_id                   = "${var.subnet_id}"
+  associate_public_ip_address = true  
+  
+  iam_instance_profile = "${var.iam_instance_profile}"
+  key_name = "GithubActions"
+  
+  disable_api_termination = "false"
+  
+   root_block_device {
+    volume_size           = "10"
+    volume_type           = "gp2"
+    delete_on_termination = "false"
+	tags = {
+              Name = "Github Actions EC2 Volume"
+             }
+  }
+  
+  tags = {
+		"Environment" = "${var.prefix}"
+		"Scope"       = "devops"
+		"Name" = "GithubActions-ec2"
+		}  
+  
+  
 }
