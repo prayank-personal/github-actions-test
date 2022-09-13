6d2c906ee93d741638da1c399ae5c5a38e54e729
prayank-personal <109146668+prayank-personal@users.noreply.github.com>
2022-09-13 18:33:52 +0530

    Update provision-ec2.tf

--- ec2-terraform/provision-ec2.tf
+++ ec2-terraform/provision-ec2.tf
@@ -23,31 +23,6 @@ filter {
 owners = ["amazon"]
 }
 
-resource "aws_iam_instance_profile" "EC2InstanceProfile" {
-  name = "${var.prefix}-EC2InstanceProfile"
-  role = "${aws_iam_role.EC2Role.name}"
-}
-
-resource "aws_iam_role" "EC2Role" {
-  name = "${var.prefix}-EC2role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
 
 resource "aws_instance" "GithubActions" {
   ami           = "${data.aws_ami.amazon-linux-2.id}"
@@ -60,7 +35,6 @@ resource "aws_instance" "GithubActions" {
   subnet_id                   = "${var.subnet_id}"
   associate_public_ip_address = true  
   
-  iam_instance_profile = "${var.iam_instance_profile}"
   key_name = "GithubActions"
   
   disable_api_termination = "false"
